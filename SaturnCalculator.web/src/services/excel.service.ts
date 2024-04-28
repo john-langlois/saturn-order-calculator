2906dc5c43c57d7709a5cc02afb67bf019d56f1a
john-langlois <joao.langlois@hotmail.com>
2024-04-27 22:34:54 -0400

    formatted excel to match template

--- SaturnCalculator.web/src/services/excel.service.ts
+++ SaturnCalculator.web/src/services/excel.service.ts
@@ -18,26 +18,38 @@ export class ExcelService {
 		  propertiesData.push([item.key, item.value]);
 		});
 	
-		// Add line items
-		const headers = ['Serial No', 'Vendor Item Number', 'Shipped Quantity', 'Item Cost', 'Total Cost'];
-		const lineItemsData = [headers];
+		// INVOICE SHEET
+		const invoiceHeaders = ['CNTBTCH','CNTITEM','IDCUST','IDINV','IDSHPT','SPECINST','TEXTTRX','IDTRX','ORDRNBR','CUSTPO','INVDESC','DATEINVC','CODECURN','EXCHRATEHC','TERMCODE','INVCTYPE'];
+		const invoiceData = [invoiceHeaders];
 		lineItems.forEach((item:any) => {
-		  lineItemsData.push([item.serialNo, item.vendorItemNumber, item.shippedQuantity, item.itemCost, item.totalCost]);
+			invoiceData.push([item.serialNo, item.vendorItemNumber, item.shippedQuantity, item.itemCost, item.totalCost]);
 		});
 
 		// Add Vendor Items
-		const vendorItemHeaders = ['Vendor Item Number', 'Total Quantity','Total Cost'];
-		const vendorItemsData = [vendorItemHeaders];
+		const invoiceDetailsHeader = ['CNTBTCH','CNTITEM','CNTLINE','IDITEM','IDDIST','TEXTDESC','UNITMEAS','QTYINVC','AMTCOST','AMTPRIC','AMTEXTN','AMTCOGS','COMMENT'];
+		const invoiceDetailsData = [invoiceDetailsHeader];
 		vendorItems.forEach((item:any) => {
-		  vendorItemsData.push([item.vendorItemNumber, item.totalQuantity, item.totalCost]);
+			invoiceDetailsData.push([0,1,item.totalQuantity,item.vendorItemNumber, item.vendorItemNumber,'',0,(item.totalCost/item.totalQuantity)]);
 		});
 
-		let appendedProperties = propertiesData.concat(lineItemsData);
-		let allData = appendedProperties.concat(vendorItemsData);
+		// Add Vendor Items
+		const invoicePaymentHeader = ['CNTBTCH','CNTITEM','IDITEM','IDDIST','TEXTDESC','UNITMEAS','QTYINVC','AMTCOST','AMTPRIC','AMTEXTN','AMTCOGS','COMMENT'];
+		const invoicePaymentData = [invoiceDetailsHeader];
+		vendorItems.forEach((item:any) => {
+			invoicePaymentData.push([item.vendorItemNumber, item.totalQuantity, item.totalCost]);
+		});
+
+		
 	
 		// Add line items to worksheet
-		const allDataWs: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(allData);
-		XLSX.utils.book_append_sheet(wb, allDataWs, 'All Data');
+		const invoices: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(invoiceData);
+		XLSX.utils.book_append_sheet(wb, invoices, 'Invoices');
+
+		const invoiceDetails: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(invoiceDetailsData);
+		XLSX.utils.book_append_sheet(wb, invoiceDetails, 'Invoice_Details');
+
+		const invoicePaymentSchedules: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(invoicePaymentData);
+		XLSX.utils.book_append_sheet(wb, invoicePaymentSchedules, 'Invoice_Payment_Schedules');
 	
 		// Save the workbook as an Excel file
 		XLSX.writeFile(wb, filename + '.xlsx');
