dc2fa715582264f2032695e7a7a29d2b5c5662ea
john-langlois <joao.langlois@hotmail.com>
2024-05-21 23:51:04 +0200

    get data from SQL

--- SaturnCalculator.lib/Repositories/SQLRepository.cs
+++ SaturnCalculator.lib/Repositories/SQLRepository.cs
@@ -54,63 +54,63 @@ namespace SaturnCalculator.lib.Repositories
         {
             try
             {
-                // using IDbConnection db = new SqlConnection(ConnectionString);
-                // return await db.QueryAsync<OrderInfo>("[ref].p_OrderInfo_GET", commandType: CommandType.StoredProcedure);
-                return new List<OrderInfo>()
-                {
-                    new OrderInfo()
-                    {
-                        ID = 1,
-                        PartNo = "3394",
-                        PPU = 12.7086,
-                        CompanyCode = "1151",
-                        CompanyName = "Formet",
-                        JobNo = "PR0029",
-                        PoNo = "5500115185",
-                        CoilBlankPrice = 0.8795,
-                        CoilBlankUnitType = "kg",
-                        PartName = "Bottom Housing"
-                    },
-                    new OrderInfo()
-                    {
-                        ID = 2,
-                        PartNo = "3393",
-                        PPU = 8.1864,
-                        CompanyCode = "1151",
-                        CompanyName = "Formet",
-                        JobNo = "PR0028",
-                        PoNo = "5500115184",
-                        CoilBlankPrice = 0.8735,
-                        CoilBlankUnitType = "kg",
-                        PartName = "Top Shield"
-                    },
-                    new OrderInfo()
-                    {
-                        ID = 3,
-                        PartNo = "G2104",
-                        PPU = 9.5191,
-                        CompanyCode = "1151",
-                        CompanyName = "Formet",
-                        JobNo = "PR0033",
-                        PoNo = "5500167707",
-                        CoilBlankPrice = 0.6320,
-                        CoilBlankUnitType = "lb",
-                        PartName = "Reinf Quarter Panel RH"
-                    },
-                    new OrderInfo()
-                    {
-                        ID = 4,
-                        PartNo = "G2105",
-                        PPU = 9.5188,
-                        CompanyCode = "1151",
-                        CompanyName = "Formet",
-                        JobNo = "PR0033",
-                        PoNo = "5500167708",
-                        CoilBlankPrice = 0.6320,
-                        CoilBlankUnitType = "lb",
-                        PartName = "Reinf Quarter Panel LH"
-                    }
-                };
+                using IDbConnection db = new SqlConnection(ConnectionString);
+                return await db.QueryAsync<OrderInfo>("[ref].p_OrderInfo_GET", commandType: CommandType.StoredProcedure);
+                // return new List<OrderInfo>()
+                // {
+                //     new OrderInfo()
+                //     {
+                //         ID = 1,
+                //         PartNo = "3394",
+                //         PPU = 12.7086,
+                //         CompanyCode = "1151",
+                //         CompanyName = "Formet",
+                //         JobNo = "PR0029",
+                //         PoNo = "5500115185",
+                //         CoilBlankPrice = 0.8795,
+                //         CoilBlankUnitType = "kg",
+                //         PartName = "Bottom Housing"
+                //     },
+                //     new OrderInfo()
+                //     {
+                //         ID = 2,
+                //         PartNo = "3393",
+                //         PPU = 8.1864,
+                //         CompanyCode = "1151",
+                //         CompanyName = "Formet",
+                //         JobNo = "PR0028",
+                //         PoNo = "5500115184",
+                //         CoilBlankPrice = 0.8735,
+                //         CoilBlankUnitType = "kg",
+                //         PartName = "Top Shield"
+                //     },
+                //     new OrderInfo()
+                //     {
+                //         ID = 3,
+                //         PartNo = "G2104",
+                //         PPU = 9.5191,
+                //         CompanyCode = "1151",
+                //         CompanyName = "Formet",
+                //         JobNo = "PR0033",
+                //         PoNo = "5500167707",
+                //         CoilBlankPrice = 0.6320,
+                //         CoilBlankUnitType = "lb",
+                //         PartName = "Reinf Quarter Panel RH"
+                //     },
+                //     new OrderInfo()
+                //     {
+                //         ID = 4,
+                //         PartNo = "G2105",
+                //         PPU = 9.5188,
+                //         CompanyCode = "1151",
+                //         CompanyName = "Formet",
+                //         JobNo = "PR0033",
+                //         PoNo = "5500167708",
+                //         CoilBlankPrice = 0.6320,
+                //         CoilBlankUnitType = "lb",
+                //         PartName = "Reinf Quarter Panel LH"
+                //     }
+                // };
             }
             catch (Exception e)
             {
