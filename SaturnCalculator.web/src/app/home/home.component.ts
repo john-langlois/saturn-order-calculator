8a7378a215eb73c87db2fa52a146b3d29cef5603
john-langlois <joao.langlois@hotmail.com>
2024-06-05 15:27:31 +0200

    pagination added to table

--- SaturnCalculator.web/src/app/home/home.component.ts
+++ SaturnCalculator.web/src/app/home/home.component.ts
@@ -18,14 +18,20 @@ export class HomeComponent implements OnInit {
 
   public selectedItem!:number;
 
+  public pageConfig:any = {
+    pageNo:1,
+    pageSize:15,
+    total:0
+  };
+
   public dataColumns:any[]=[
     {name:"PO Number",source:"poNumber", type:"text"},
     {name:"Trading Partner",source:"tradingPartner", type:"text"},
     {name:"Bill Landing No",source:"billLandingNo", type:"text"},
     {name:"Tracking No",source:"trackingNo", type:"text"},
     {name:"Ship Date",source:"shipDate", type:"text"},
-    {name:"Ship Time",source:"shipTime", type:"date"},
-    {name:"Package Type",source:"packageType", type:"date"},
+    {name:"Ship Time",source:"shipTime", type:"text"},
+    {name:"Package Type",source:"packageType", type:"text"},
     {name:"Total Package Count",source:"totalPackageCount", type:"text"},
     {name:"Order Quantity",source:"orderQuantity", type:"text"},
     {name:"Order Cost",source:"orderCost", type:"text"}
@@ -41,6 +47,7 @@ export class HomeComponent implements OnInit {
 
   public async getOrders(){
     this.orders = await this.orderService.GetAllOrders();
+    this.pageConfig.total = this.orders.length;
   }
 
   public toggleSelectedItem(id:number){
